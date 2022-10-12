b80180fb7837d0c48c6576d2175defe6c900df9d
Guangguang Ma <ggma@thoughtworks.com>
2022-10-12 10:49:47 +0800

    test(question3): verify printed content is from the processed purchase

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,3 +1,5 @@
+using Moq;
+
 namespace CashRegisterTest
 {
 	using CashRegister;
@@ -16,6 +18,19 @@ namespace CashRegisterTest
 			cashRegister.Process(purchase);
 			//then
 			Assert.NotEmpty(printer.PrintContent);
+		}
+
+		[Fact]
+		public void Should_process_execute_printing_with_the_content_from_the_processed_Purchase()
+		{
+			//given
+			var printer = new MockPrinter();
+			var cashRegister = new CashRegister(printer);
+			var purchase = new Purchase();
+			//when
+			cashRegister.Process(purchase);
+			//then
+			Assert.StartsWith("content", printer.PrintContent);
 		}
 	}
 }
