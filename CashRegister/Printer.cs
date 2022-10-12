bb675a9d96eb5e7a6392e6cc3672fe9332fa0cd6
Guangguang Ma <ggma@thoughtworks.com>
2022-10-12 10:43:06 +0800

    test(question1): verify Print() is triggered by Process()

--- CashRegister/Printer.cs
+++ CashRegister/Printer.cs
@@ -1,9 +1,14 @@
+using System;
+
 namespace CashRegister
 {
-	public class Printer
+	public class Printer : IPrinter
 	{
+		public string PrintContent { get; private set; }
+
 		public void Print(string content)
 		{
+			this.PrintContent = content;
 			// send message to a real printer
 		}
 	}
