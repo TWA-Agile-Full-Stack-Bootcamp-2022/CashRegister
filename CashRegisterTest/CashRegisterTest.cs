c50cdf8f0258b20d7b677354e6126038089f709d
Hong Wu <hong.wu@rea-group.com>
2023-10-07 15:36:34 +0800

    test: when the Process(...) method is called, the Print() method should also be called.

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,20 +1,24 @@
 namespace CashRegisterTest
 {
-	using CashRegister;
-	using Xunit;
+    using CashRegister;
+    using Moq;
+    using Xunit;
 
-	public class CashRegisterTest
-	{
-		[Fact]
-		public void Should_process_execute_printing()
-		{
-			//given
-			var cashRegister = new CashRegister();
-			var purchase = new Purchase();
-			//when
-			cashRegister.Process(purchase);
-			//then
-			//verify that cashRegister.process will trigger print
-		}
-	}
+    public class CashRegisterTest
+    {
+        [Fact]
+        public void Should_process_execute_printing()
+        {
+            //given
+            var printer = new Mock<IPrinter>();
+            CashRegister cashRegister = new CashRegister(printer.Object);
+            Purchase purchase = new Purchase();
+
+            //when
+            cashRegister.Process(purchase);
+
+            //then
+            printer.Verify(p => p.Print(It.IsAny<string>()), Times.Once());
+        }
+    }
 }
