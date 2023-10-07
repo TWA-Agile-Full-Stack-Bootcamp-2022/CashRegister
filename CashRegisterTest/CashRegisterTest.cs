8765c2aa3f432a3246aedcefec4e10c9e08b443a
Hong Wu <hong.wu@rea-group.com>
2023-10-07 15:42:58 +0800

    test: when Print() is triggered by Process(), the printed content is from the processed Purchase

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -20,5 +20,20 @@ namespace CashRegisterTest
             //then
             printer.Verify(p => p.Print(It.IsAny<string>()), Times.Once());
         }
+
+        [Fact]
+        public void Printed_content_should_be_from_the_processed_purchase()
+        {
+             //given
+            var printer = new Mock<IPrinter>();
+            var cashRegister = new CashRegister(printer.Object);
+            var purchase = new Purchase();
+
+            //when
+            cashRegister.Process(purchase);
+
+            //then
+            printer.Verify(p => p.Print(It.Is<string>(content => content.Contains("content"))), Times.Once());
+        }
     }
 }
