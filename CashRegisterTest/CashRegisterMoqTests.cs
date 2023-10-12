b795a603a95e2d56686090677ce3b695a803701e
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-12 16:40:04 +0800

    test: Should_pass_the_purchase_info_to_printer_by_given_Purchase_Object with Moq

--- CashRegisterTest/CashRegisterMoqTests.cs
+++ CashRegisterTest/CashRegisterMoqTests.cs
@@ -2,7 +2,6 @@
 {
     using CashRegister;
     using Moq;
-    using System;
     using Xunit;
 
     public class CashRegisterMoqTests
@@ -19,6 +18,27 @@
             //then
             //verify that cashRegister.process will trigger print
             mockedPrinter.Verify(printer => printer.Print(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void Should_pass_the_purchase_info_to_printer_by_given_Purchase_Object()
+        {
+            // given
+            var mockedPrinter = new Mock<Printer>();
+            var cashRegister = new CashRegister(mockedPrinter.Object);
+
+            const string givenPurchaseInfo = "Some Given Purchase infomation";
+            //StubPurchase strubPurchase = new StubPurchase();
+            //strubPurchase.WhenAsStringThenReturn(givenPurchaseInfo);
+
+            var mockedPurchase = new Mock<Purchase>();
+            mockedPurchase.Setup(purchase => purchase.AsString()).Returns(givenPurchaseInfo);
+
+            // when
+            cashRegister.Process(mockedPurchase.Object);
+            // then
+            mockedPrinter.Verify(printer => printer.Print(givenPurchaseInfo), Times.Once);
+            //Assert.Equal(givenPurchaseInfo, mockedPrinter.PrintContentRecived);
         }
     }
 }
