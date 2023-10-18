ad2b308097135f2604589c99773179b0c82cb860
yoogodong <yoogdong@gmail.com>
2023-10-18 12:35:32 +0800

    feat: for question 1~3

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,20 +1,23 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
-	using Xunit;
+    using Moq;
+    using Xunit;
 
 	public class CashRegisterTest
 	{
 		[Fact]
 		public void Should_process_execute_printing()
 		{
-			//given
-			var cashRegister = new CashRegister();
-			var purchase = new Purchase();
-			//when
-			cashRegister.Process(purchase);
-			//then
-			//verify that cashRegister.process will trigger print
+            //given
+            Mock<Printer> mock = new Mock<Printer>();
+            var cashRegister = new CashRegister(mock.Object);
+            Mock<Purchase> mockPurchase = new Mock<Purchase>();
+            mockPurchase.Setup(p => p.AsString()).Returns("1234");
+            //when
+            cashRegister.Process(mockPurchase.Object);
+            //then
+            mock.Verify(x => x.Print("1234"));
 		}
 	}
 }
